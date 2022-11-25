eec5e05717013b72e6650b98a305c89a97d1b513
kevin-maciel <kevin.maciel@moxit.net>
2022-11-25 14:11:40 -0300

    delete sections 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,26 +5,12 @@ import Page from '../components/Page';
 // sections
 import {
   HomeHero,
-  HomeMinimal,
-  HomeDarkMode,
-  HomeLookingFor,
-  HomeColorPresets,
-  HomePricingPlans,
-  HomeAdvertisement,
-  HomeCleanInterfaces,
-  HomeHugePackElements,
 } from '../sections/home';
 
 // ----------------------------------------------------------------------
 
 const RootStyle = styled('div')(() => ({
   height: '100%',
-}));
-
-const ContentStyle = styled('div')(({ theme }) => ({
-  overflow: 'hidden',
-  position: 'relative',
-  backgroundColor: theme.palette.background.default,
 }));
 
 // ----------------------------------------------------------------------
@@ -34,23 +20,6 @@ export default function HomePage() {
     <Page title="The starting point for your next project">
       <RootStyle>
         <HomeHero />
-        <ContentStyle>
-          <HomeMinimal />
-
-          <HomeHugePackElements />
-
-          <HomeDarkMode />
-
-          <HomeColorPresets />
-
-          <HomeCleanInterfaces />
-
-          <HomePricingPlans />
-
-          <HomeLookingFor />
-
-          <HomeAdvertisement />
-        </ContentStyle>
       </RootStyle>
     </Page>
   );
